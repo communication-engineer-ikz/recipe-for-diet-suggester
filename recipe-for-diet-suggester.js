5f990772c9fa2bc338c21a0b237525f06fae168f
ikz <75650546+communication-engineer-ikz@users.noreply.github.com>
2021-07-10 08:59:08 +0900

    文字列はダブルクオートで囲む派

--- recipe-for-diet-suggester.js
+++ recipe-for-diet-suggester.js
@@ -1,6 +1,6 @@
 function recipeForDietSuggester() {
     const ACCESS_TOKEN = getAccessToken();
-    const line_endpoint = 'https://api.line.me/v2/bot/message/reply';
+    const line_endpoint = "https://api.line.me/v2/bot/message/reply";
     const replyContent = "沼";
    
     if(replyContent == "") {
@@ -9,20 +9,20 @@ function recipeForDietSuggester() {
     } else {
 
         UrlFetchApp.fetch(line_endpoint, {
-            'headers': {
-                'Content-Type': 'application/json; charset=UTF-8',
-                'Authorization': 'Bearer ' + ACCESS_TOKEN,
+            "headers": {
+                "Content-Type": "application/json; charset=UTF-8",
+                "Authorization": "Bearer " + ACCESS_TOKEN,
             },
-            'method': 'post',
-            'payload': JSON.stringify({
-                'replyToken': reply_token,
-                'messages': [{
-                    'type': 'text',
-                    'text': replyContent,
+            "method": "post",
+            "payload": JSON.stringify({
+                "replyToken": reply_token,
+                "messages": [{
+                    "type": "text",
+                    "text": replyContent,
                 }],
             }),
         });
 
-        return ContentService.createTextOutput(JSON.stringify({'content': 'post ok'})).setMimeType(ContentService.MimeType.JSON);
+        return ContentService.createTextOutput(JSON.stringify({"content": "post ok"})).setMimeType(ContentService.MimeType.JSON);
     }
 }
